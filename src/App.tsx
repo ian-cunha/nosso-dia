5cc1cf7a8b0d8778ed97b6f84c27878692248e10
Ian <iancunha20@gmail.com>
2025-03-03 02:12:36 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RelationshipCounter from './RelationshipCounter';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <RelationshipCounter />
+    </div>
+  );
+};
+
+export default App;
